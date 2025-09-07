d75abc9806d49a25ef9cce0178d433aa6621fae8
Ranjit Rajbansi <ranjitrajbanshi158@gmail.com>
2025-09-07 19:39:23 +0545

    Form validation through form request

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -27,18 +27,7 @@ class FormController extends Controller
             'age' => 'required|numeric|between:18,30',
         ],
         [
-            'name.required' => "Name is required",
-            'name.alpha' => 'Name must be alphabetic',
-            'email.required' => "Email is required",
-            'email.email' => 'Email must be a valid email address',
-            'email.unique' => 'Email has already been taken',
-            'password.required' => 'Password is required',
-            'password.min' => 'Password must be minimum 8 characters',
-            'password.max' => 'Password must be maximum 14 characters',
-            'password.regex' => 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character',
-            'age.required' => 'Age is required',
-            'age.numeric' => 'Age must be a number',
-            'age.between' => 'Age must be between 18 and 30',
+            
         ]);
 
         Form::create($form);
